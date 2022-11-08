dc9013ec5acbf2cc54850e8f69e430d619a2c2b8
kuvuś <kuba17ism@tuta.io>
2022-11-08 22:35:25 +0100

    Add search box mockup

---
+++ components/search/SearchBox.js
@@ -0,0 +1,22 @@
+'use client'
+
+import Button from '../common/Button'
+import styles from '../../styles/search/SearchBox.module.scss'
+import { useRef } from 'react'
+
+function SearchBox(props) {
+    const inputEl = useRef()
+
+    const handleBoxClick = () => {
+        inputEl.current.focus()
+    }
+
+    return (
+        <div className={styles.searchBox} onClick={handleBoxClick}>
+            <input ref={inputEl} type='text' placeholder={'Czego szukasz?'} />
+            <Button element={'button'}>Szukaj!</Button>
+        </div>
+    )
+}
+
+export default SearchBox
