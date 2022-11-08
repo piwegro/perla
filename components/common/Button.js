bc1dff469d50741d4001a049aee586860310cdc5
kuvuś <kuba17ism@tuta.io>
2022-11-08 22:32:29 +0100

    Fix button styling

--- components/common/Button.js
+++ components/common/Button.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import Link from 'next/link'
+import openSans from '../../styles/common/Font'
 
 import styles from '../../styles/common/Button.module.scss'
 
 function Button(props) {
-    const { type, href, children } = props
+    const { element, href, children } = props
 
-    switch (type) {
+    switch (element) {
         case 'anchor':
             return (
-                <Link className={styles.button} href={href || '/'}>
+                <Link
+                    className={`${styles.button} ${openSans.className}`}
+                    href={href || '/'}
+                    {...props}>
                     {children}
                 </Link>
             )
         case 'button':
             return (
-                <button className={styles.button} {...props}>
+                <button className={`${styles.button} ${openSans.className}`} {...props}>
                     {children}
                 </button>
             )
