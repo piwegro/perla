1a924a3055bab5032dbd86e231f8e5ea1aada538
kuvuś <kuba17ism@tuta.io>
2022-11-18 00:16:38 +0100

    Update offer page

--- src/app/(offers)/offer/[id]/page.js
+++ src/app/(offers)/offer/[id]/page.js
@@ -1,8 +1,13 @@
 import Hero from '../../../../components/common/Hero'
+import styles from '../../../../styles/pages/Offer.module.scss'
+import { container } from '../../../../styles/common/Grid.module.scss'
+import Button from '../../../../components/common/Button'
+import Link from 'next/link'
 
 const getOffer = async id => {
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/offer/${id}`)
 
+    // TODO: replace this with some json return
     if (!res.ok) {
         throw new Error(res.statusText)
     }
@@ -19,6 +24,31 @@ const Page = async ({ params }) => {
             <Hero center={true}>
                 <h2>{offerData.title}</h2>
             </Hero>
+            <div className={container}>
+                <div className={styles.wrapper}>
+                    <div className={`${styles.box} ${styles.mainBox}`}>a</div>
+                    <div className={`${styles.box} ${styles.userBox}`}>
+                        <div className={styles.sellerDetails}>
+                            <img
+                                src='/user_avatar.png'
+                                alt='Avatar użytkownika'
+                                className={styles.sellerAvatar}
+                            />
+                            <h3>{offerData.seller.name}</h3>
+                            <Button element={'anchor'} href={'/'}>
+                                Wyślij wiadomość
+                            </Button>
+
+                            <Link
+                                href={`/user/${offerData.seller.id}`}
+                                className={styles.otherOffers}>
+                                Inne ogłoszenia od tej osoby
+                            </Link>
+                        </div>
+                    </div>
+                    <div className={`${styles.box} ${styles.mainBox}`}>c</div>
+                </div>
+            </div>
         </>
     )
 }
