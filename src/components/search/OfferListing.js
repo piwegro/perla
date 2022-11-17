6f1cc9df6e94381abed4e233071257b293d70a97
kuvuś <kuba17ism@tuta.io>
2022-11-18 00:16:50 +0100

    Update values in OfferListing.js

--- src/components/search/OfferListing.js
+++ src/components/search/OfferListing.js
@@ -7,15 +7,17 @@ const OfferListing = props => {
     return (
         <div className={styles.offerListing} {...props}>
             <div className={styles.offerDesc}>
-                <img src={data?.photo} alt='' />
+                <img src={data?.photo ?? 'https://place-hold.it/96'} alt='' />
                 <div>
-                    <b>{data?.name}</b>
-                    <span>{data?.description}</span>
+                    <b>{data?.title ?? '-'}</b>
+                    <span>{data?.description ?? '-'}</span>
                 </div>
             </div>
             <div className={styles.offerRight}>
-                <div className={styles.price}>{data?.price === '0' ? 'Za darmo' : '?'}</div>
-                <Button element={'anchor'} href={`/offer/${data?.id}`}>
+                <div className={styles.price}>
+                    {data?.price?.amount ?? '-'} {data?.price?.currency?.symbol}
+                </div>
+                <Button element={'anchor'} href={`/offer/${data?.id ?? 1}`}>
                     Zobacz ogłoszenie
                 </Button>
             </div>
