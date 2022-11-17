2450aacc6ccf1a792f46e300aa39b4db4ab12bd8
kuvuś <kuba17ism@tuta.io>
2022-11-18 00:15:53 +0100

    Add fetch to last offers

--- src/components/search/LastOffers.js
+++ src/components/search/LastOffers.js
@@ -1,10 +1,22 @@
 import OfferList from './OfferList'
 
-const LastOffers = () => {
-    // TODO: This is only a placeholder. Final version should fetch those items from API
+const getLastOffers = async () => {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/offers/0`)
+
+    // TODO: replace this with some json return
+    if (!res.ok) {
+        throw new Error(res.statusText)
+    }
+
+    return res.json()
+}
+
+const LastOffers = async () => {
+    const lastOffers = await getLastOffers()
+
     return (
         <>
-            <OfferList />
+            <OfferList offers={lastOffers} />
         </>
     )
 }
--- src/components/search/OfferList.js
+++ src/components/search/OfferList.js
@@ -1,29 +1,12 @@
 import OfferListing from './OfferListing'
 import styles from '../../styles/search/OfferList.module.scss'
 
-const OfferList = () => {
-    const offers = [
-        // TODO: This is only a placeholder. Final version should get items from props
+const OfferList = offers => {
+    offers = offers?.offers ?? [
         {
-            id: '123',
-            name: 'Czajnik elektryczny',
-            description: 'Oddam czajnik',
-            photo: 'https://place-hold.it/96',
-            price: '0',
-            currency: 'beer',
-        },
-        {
-            id: '123',
-            name: 'Czajnik elektryczny',
-            description: 'Oddam czajnik',
-            photo: 'https://place-hold.it/96',
-            price: '0',
-            currency: 'beer',
-        },
-        {
-            id: '123',
-            name: 'Czajnik elektryczny',
-            description: 'Oddam czajnik',
+            id: '0',
+            title: 'Brak ofert',
+            description: '',
             photo: 'https://place-hold.it/96',
             price: '0',
             currency: 'beer',
