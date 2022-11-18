f5a5a263cf4e88b347cfbb0c130bc973bef6c5e9
kuvuś <kuba17ism@tuta.io>
2022-11-18 14:35:52 +0100

    Add request to set user displayName and to add the user to the DB

--- src/app/user/auth/signup/page.js
+++ src/app/user/auth/signup/page.js
@@ -1,25 +1,64 @@
 'use client'
 
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { initFirebase } from '../../../../utils/firebase'
 import { getAuth } from 'firebase/auth'
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth'
+import { useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth'
 import Button from '../../../../components/common/Button'
 import stylesCommon from '../../../../styles/pages/auth/AuthCommon.module.scss'
 import Link from 'next/link'
+import fetch from 'node-fetch'
 
 function Page(props) {
     initFirebase()
     const auth = getAuth()
     const [createUserWithEmailAndPassword, user, loading, error] =
-        useCreateUserWithEmailAndPassword(auth)
+        useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true })
+    const [updateProfile, updating, updateError] = useUpdateProfile(auth)
+    const [dbAddError, setDbAddError] = useState(false)
 
+    const usernameField = useRef(null)
     const emailField = useRef(null)
     const passwordField = useRef(null)
 
-    const handleSubmit = () => {
-        console.log(emailField.current.value, passwordField.current.value)
-        createUserWithEmailAndPassword(emailField.current.value, passwordField.current.value)
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        const [username, email, password] = [
+            usernameField.current.value,
+            emailField.current.value,
+            passwordField.current.value,
+        ]
+
+        if (!(username && email && password)) {
+            // TODO: add error, empty field
+            return
+        }
+
+        createUserWithEmailAndPassword(emailField.current.value, passwordField.current.value).then(
+            userData => {
+                if (!loading && !error)
+                    updateProfile({ displayName: username }).then(() => {
+                        addUserToDatabase(userData.user)
+                    })
+            }
+        )
+    }
+
+    const addUserToDatabase = user => {
+        fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/${user.uid}`, {
+            method: 'PUT',
+        })
+            .then(res => {
+                if (!res.ok) setDbAddError(true)
+                return res.json()
+            })
+            .then(res => {
+                console.log(res)
+            })
+            .catch(e => {
+                setDbAddError(true)
+            })
     }
 
     // Firebase error codes
@@ -27,16 +66,18 @@ function Page(props) {
     // auth/invalid-email - email is invalid
     // auth/weak-password - (min 6 characters)
 
+    // TODO: add errors for creating user (error), updating user (updateError), adding to database (dbAddError)
+
     return (
         <div className={stylesCommon.wrapper}>
             <div className={stylesCommon.content}>
                 <div className={stylesCommon.authBox}>
                     <h2>Rejestracja</h2>
-                    <form
-                        onSubmit={e => {
-                            e.preventDefault()
-                            handleSubmit()
-                        }}>
+                    <form onSubmit={handleSubmit}>
+                        <div className={stylesCommon.formGroup}>
+                            <label htmlFor='username'>Nazwa użytkownika</label>
+                            <input type={'text'} id={'username'} ref={usernameField} />
+                        </div>
                         <div className={stylesCommon.formGroup}>
                             <label htmlFor='email'>Adres e-mail</label>
                             <input type={'email'} id={'email'} ref={emailField} />
