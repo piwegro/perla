0fee2fc0d153255545480d5b5114d3f2533c97d1
kuvuś <kuba17ism@tuta.io>
2022-11-29 00:21:30 +0100

    Make search box work

--- src/components/search/SearchBox.js
+++ src/components/search/SearchBox.js
@@ -3,9 +3,16 @@
 import Button from '../common/Button'
 import styles from '../../styles/search/SearchBox.module.scss'
 import { useRef } from 'react'
+import { useRouter } from 'next/navigation'
 
 const SearchBox = () => {
     const inputEl = useRef()
+    const router = useRouter()
+
+    const handleSubmit = () => {
+        if (inputEl.current.value.length > 0)
+            router.push(`/search/${encodeURI(inputEl.current.value)}`)
+    }
 
     const handleBoxClick = () => {
         inputEl.current.focus()
@@ -14,7 +21,9 @@ const SearchBox = () => {
     return (
         <div className={styles.searchBox} onClick={handleBoxClick}>
             <input ref={inputEl} type='text' placeholder={'Czego szukasz?'} />
-            <Button element={'button'}>Szukaj!</Button>
+            <Button element={'button'} onClick={handleSubmit}>
+                Szukaj!
+            </Button>
         </div>
     )
 }
