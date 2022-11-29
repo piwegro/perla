4c5c75ec169ec6f89c55c3952be9591efbc99072
kuvuś <kuba17ism@tuta.io>
2022-11-29 14:33:30 +0100

    Update styles

--- src/components/search/SearchBox.js
+++ src/components/search/SearchBox.js
@@ -9,7 +9,9 @@ const SearchBox = () => {
     const inputEl = useRef()
     const router = useRouter()
 
-    const handleSubmit = () => {
+    const handleSubmit = e => {
+        e.preventDefault()
+
         if (inputEl.current.value.length > 0)
             router.push(`/search/${encodeURI(inputEl.current.value)}`)
     }
@@ -20,10 +22,12 @@ const SearchBox = () => {
 
     return (
         <div className={styles.searchBox} onClick={handleBoxClick}>
-            <input ref={inputEl} type='text' placeholder={'Czego szukasz?'} />
-            <Button element={'button'} onClick={handleSubmit}>
-                Szukaj!
-            </Button>
+            <form>
+                <input ref={inputEl} type='text' placeholder={'Czego szukasz?'} />
+                <Button element={'button'} onClick={handleSubmit} type={'submit'}>
+                    Szukaj!
+                </Button>
+            </form>
         </div>
     )
 }
